3b53380a787eae93d45e889d5332c48cb5c7fb00
Harvey Ramos <harvey@harvsworld.com>
2015-08-29 19:00:09 +0000

    Bower setup. trying out react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,42 +4,82 @@
         <title>Laravel</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" />
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/JSXTransformer.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min.js"></script>
+        
+        <script src="{{ asset('/js/vendor.js') }}"></script>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+            <div id="content"></div>
+            <script type="text/jsx">
+                var CommentBox = React.createClass({
+                    render: function() {
+                        return (
+                            <div className="commentBox">
+                                <h1>CommentBox</h1>
+                                <CommentList data={this.props.data} />
+                                <CommentForm />
+                            </div>
+                        );
+                    }
+                });
+                
+                var CommentList = React.createClass({
+                    render: function() {
+                        var commentNodes = this.props.data.map(function (comment) {
+                            return (
+                                <Comment author={comment.author}>
+                                {comment.text}
+                                </Comment>
+                            );
+                        });
+                        return (
+                            <div className="commentList">
+                                {commentNodes}
+                            </div>
+                        );
+                    }
+                });
+                
+                var CommentForm = React.createClass({
+                    render: function() {
+                        return (
+                            <div className="commentForm">
+                                This is a CommentForm
+                            </div>
+                        );
+                    }
+                });
+                
+                var Comment = React.createClass({
+                    render: function() {
+                        var rawComment = marked(this.props.children.toString(), {sanitize: true});
+                        return (
+                            <div className="Comment">
+                                <h2 className="commentAuthor">
+                                    { this.props.author }
+                                </h2>
+                                <span dangerouslySetInnerHTML=@{{ __html: rawComment }} />
+                            </div>
+                        );
+                    }
+                });
+                
+                var data = [
+                    { author: "Pete Hunt", text: "This is one comment" },
+                    { author: "Jordan Walke", text: "This is *another* comment" }
+                ];
+                
+                React.render(
+                    <CommentBox data={data} />,
+                    document.getElementById('content')
+                );
+            </script>
         </div>
     </body>
 </html>
