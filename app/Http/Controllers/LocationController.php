e5b3d3cba71d03705edb6e32ea599f23ed551348
Harvey Ramos <harvey@harvsworld.com>
2015-09-02 03:36:04 +0000

    Redid controllers, added Resource Routes for location and price. Added migrations for locations and price

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class location extends Controller
+class LocationController extends Controller
 {
     /**
      * Display a listing of the resource.
---
+++ database/migrations/2015_09_02_020425_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->string('address');
+            $table->integer('type');
+            $table->text('details');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations');
+    }
+}
