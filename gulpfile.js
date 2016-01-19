4684493b01ec2c51ac87ebfd5a2fbcc3e1fef689
Harvey Ramos <harvey@harvsworld.com>
2016-01-18 21:52:45 -0500

    Update composer, bower, npm. Gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,30 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
-    mix.scripts(
-        [
-            '../bower/bootstrap-sass/assets/javascripts/bootstrap.js'
+    // Mix Bootstrap files, only the ones needed
+    mix.scripts([
+            '../bower/bootstrap/js/dist/util.js',
+            '../bower/bootstrap/js/dist/alert.js',
+            '../bower/bootstrap/js/dist/button.js',
+            // '../bower/bootstrap/js/dist/carousel.js',
+            '../bower/bootstrap/js/dist/collapse.js',
+            '../bower/bootstrap/js/dist/dropdown.js',
+            '../bower/bootstrap/js/dist/modal.js',
+            // '../bower/bootstrap/js/dist/scrollspy.js',
+            '../bower/bootstrap/js/dist/tab.js',
+            // '../bower/bootstrap/js/dist/tooltip.js',
+            // '../bower/bootstrap/js/dist/popover.js'
         ],
-        'public/js/vendor.js'
-    );
-    mix.copy(
-        'resources/assets/bower/font-awesome/fonts', 'public/fonts'
-    );
+        'resources/assets/js/vendors/bootstrap.js');
+
+    // Concat all the vendor files together
+    mix.scriptsIn('resources/assets/js/vendors', 'public/assets/js/vendors.js');
+
+    // Compile App JS
+    mix.scripts('app.js', 'public/assets/js/app.js');
+
+    // Compile custom SASS
+    mix.sass('app.scss', 'public/assets/css/app.css');
+    // Get Font-Awesome fonts and copy to the right place
+    mix.copy('resources/assets/bower/font-awesome/fonts', 'public/assets/fonts');
 });
---
+++ resources/assets/js/app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by harvey on 1/18/2016.
+ */
