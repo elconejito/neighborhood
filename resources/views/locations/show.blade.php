abebd077fe9f6b4f2beeb97f81d70c64f8f0e629
Harvey Ramos <harvey@harvsworld.com>
2016-01-30 01:38:10 -0500

    Add Location detail page

--- resources/views/locations/show.blade.php
+++ resources/views/locations/show.blade.php
@@ -5,9 +5,34 @@
 @section('content')
     <div class="container">
 
-        <h1><small>Location</small><br />{{ $location->label }}</h1>
+        <h1>{{ $location->number }}<br /><small>{{ $location->address }}</small></h1>
         <div class="row">
-            <p></p>
+            <div class="col-md-6">
+                <h3>Prices</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Type</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ( $location->prices()->get() as $price )
+                        <tr>
+                            <td>{{ $price->price_date }}</td>
+                            <td>{{ $price->type }}</td>
+                            <td>{{ $price->price }}</td>
+                            <td><a href="{{ route('locations.prices.edit', [$location->id, $price->id]) }}" class="btn btn-secondary btn-sm"><i class="fa fa-pencil"></i></a> <a href="#" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <h3>Details</h3>
+                <p>{{ $location->details }}</p>
+            </div>
         </div>
 
     </div><!-- /.container -->
